3655decb9e13a457d10691d4a334ecec87e297aa
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-06 10:17:59 -0500

    DVD Logo changes color on bounce

--- Scripts/dvd_logo.gd
+++ Scripts/dvd_logo.gd
@@ -11,7 +11,20 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
+	#Creates a Kinematics2D object called collision
 	
 	if collision:
 		#position.x = position.x + (velocity.x * delta);
-		velocity = velocity.bounce(collision.get_normal())
+		#velocity = velocity.bounce(collision.get_normal())
+		#get_normal gets the normal against a plane (orthogonal unit vector)
+		#bounce just works here but I shouldn't just rely on Godot's methods.
+		
+		var direction = collision.get_normal()
+		if direction.y != 0:
+			velocity.y *= -1
+		
+		elif direction.x != 0:
+			velocity.x *= -1;
+			
+		#modulate = Color.from_rgb8(randi_range(0,255), randi_range(0,255), randi_range(0,255))
+		modulate = Color.from_hsv(randf_range(0,1), 0.5, 1.0)
